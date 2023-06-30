c189d801c4c3ca1aff052f64b57857491b3f5642
Alpha <piotr@piotryordanov.com>
2023-07-01 02:57:09 +0300

    feat: add example folder that contains demo strategies

---
+++ examples/macd.py
@@ -0,0 +1,64 @@
+from quantnb.strategies.S_test import S_Test
+
+from quantnb.lib import np, timeit, pd
+from quantnb.lib import find_files, optimize
+
+# df = pd.read_parquet("./binance-BTCUSDT-1h-RSI.parquet")
+# df
+#
+#
+# # |%%--%%| <o5y8KFNmnb|PSegVofl0P>
+
+
+assets = find_files("./data/", "binance")
+assets
+
+for asset in assets:
+    print(asset)
+    sym = asset.split("/")[-1].split(".")[0]
+    data = pd.read_parquet(asset)
+
+    optimisation = optimize(
+        data,
+        S_Test,
+        long=range(200, 300, 1),
+        short=range(5, 55, 1),
+        rsi=range(3, 15, 1),
+        atr_distance=np.arange(0.5, 10.5, 0.5),
+    )
+    optimisation = optimisation.sort_values("ratio", ascending=False).head(10)
+    optimisation.to_parquet(f"{sym}-RSI.parquet")
+
+# |%%--%%| <PSegVofl0P|W9pyX2nkWW>
+
+from quantnb.strategies.S_test import S_Test
+
+from quantnb.lib import np, timeit, pd
+from quantnb.lib import find_files, optimize
+
+
+assets = find_files("./data/@ENQ.time", "1h")
+data = pd.read_parquet(assets[0])
+data
+
+data = data[0:500]
+
+
+def single():
+    bt = S_Test(data)
+    bt.backtest((208, 6, 14, 10))
+    return bt
+
+
+pf = single()
+# pf.print_trades()
+pf.plot_equity()
+pf.stats
+pf.plot_ohlc()
+
+# |%%--%%| <W9pyX2nkWW|40lFm5vOxI>
+
+
+count = 30
+execution_time = timeit.timeit(single, number=count)
+print(f"Average execution time: {execution_time / count} seconds")
