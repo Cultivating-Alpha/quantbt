9bd59d2bd49aaeb582980f422d9a0d27f81d6b4a
Alpha <piotr@piotryordanov.com>
2023-08-02 20:45:07 +0400

    feat: stashing

--- examples/bid_ask/bid_ask.py
+++ examples/bid_ask/bid_ask.py
@@ -1,16 +1,16 @@
 from quantnb.strategies.S_bid_ask import S_bid_ask
 from quantnb.strategies.S_base import S_base
-from quantnb.lib import np, timeit, pd, time_manip, calculate_stats
+from quantnb.lib import np, pd, time_manip, calculate_stats
 from quantnb.core.backtester import Backtester
 from numba import njit
 from quantnb.lib.output_trades import output_trades
+from quantnb.core.enums import CommissionType, DataType
 
 
 ohlc = pd.read_parquet("./data/EURUSD.parquet")
 long = pd.read_parquet("./data/long_signals.parquet")
 short = pd.read_parquet("./data/short_signals.parquet")
 
-# |%%--%%| <U8YDjUSDd1|H1CABp95z6>
 
 INITIAL_CAPITAL = 10000
 data = ohlc[0:2000015]
@@ -20,16 +20,14 @@ data.reset_index(inplace=True)
 def backtest(data, trades, initial_capital=INITIAL_CAPITAL):
     bt = Backtester(
         commission=7,
-        commission_type="fixed",
-        initial_capital=initial_capital,
+        commission_type=CommissionType.FIXED,
+        initial_capital=INITIAL_CAPITAL,
         slippage=0.0002,
+        data_type=DataType.BID_ASK,
+        date=time_manip.convert_datetime_to_ms(data["Date"]).values,
+        bid=data["EURUSD.bid"].to_numpy(dtype=np.float32),
+        ask=data["EURUSD.ask"].to_numpy(dtype=np.float32),
     )
-    bt.set_bid_ask_data(
-        time_manip.convert_datetime_to_ms(data["Date"]).values,
-        data["EURUSD.bid"].values,
-        data["EURUSD.ask"].values,
-    )
-
     bt.from_trades(trades.values)
 
     return bt
@@ -37,25 +35,25 @@ def backtest(data, trades, initial_capital=INITIAL_CAPITAL):
 
 bt = backtest(data, long)
 
-stats = calculate_stats(data, bt)
-trades = output_trades(bt)
-# trades[:-1]
-#
-# # trades[:-1]["PNL"].sum()
-trades
-print(trades["PNL"].sum())
-# num = trades["PNL"].sum()
-# num
-# 10000 - 378
+# stats = calculate_stats(data, bt)
+# trades = output_trades(bt)
+# # trades[:-1]
+# #
+# # # trades[:-1]["PNL"].sum()
 # trades
-# # #
+# print(trades["PNL"].sum())
+# # num = trades["PNL"].sum()
+# # num
+# # 10000 - 378
 # # trades
-# # bt.closed_trades
-# bt.active_trades
-# bt.trades
-#
-# trades
-# data
+# # # #
+# # # trades
+# # # bt.closed_trades
+# # bt.active_trades
+# # bt.trades
+# #
+# # trades
+# # data
 
 
 # |%%--%%| <H1CABp95z6|uqfPit33s4>
--- quantnb/core/helpers.py
+++ quantnb/core/helpers.py
@@ -5,6 +5,7 @@ from numba import njit
 @njit
 def print_bar(length, fill, iteration, total, prev_percentage):
     percentage = iteration * 100 / total
+    print("AS")
     if percentage - prev_percentage > 10:
         progress = iteration / float(total)
         filled_length = int(length * progress)
