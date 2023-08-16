d72eb5eecc7d5fdff69f043149bb666e7242e7ba
Alpha <piotr@piotryordanov.com>
2023-08-16 22:17:36 +0400

    feat: update binance data fetch example

--- examples/fetch_binance_data.py
+++ examples/fetch_binance_data.py
@@ -1,11 +1,38 @@
 from quantnb.lib.fetch_binance_data import fetch_binance_data
-
-
-fetch_binance_data()
-
-# |%%--%%| <N7dBWmxr3c|NImzBBNtIq>
-
+from binance.client import Client
 import pandas as pd
 
-df = pd.read_parquet("data/binance-XLMUSDT-1h.parquet")
-df
+
+
+assets = [
+    "ETHUSDT",
+    # "BTCUSDT",
+    # "DOGEUSDT",
+    # "BNBUSDT",
+    # "ATOMUSDT",
+    # "NEOUSDT",
+    # "NEOUSDT",
+    # "XRPUSDT",
+    # "LTCUSDT",
+    # "TRXUSDT",
+    # "AVAXUSDT",
+    # "SHIBUSDT",
+    # "ATOMUSDT",
+    # "LINKUSDT",
+    # "UNIUSDT",
+    # "ADAUSDT",
+    # "DOTUSDT",
+    # "SOLUSDT",
+    # "MATICUSDT",
+    # "XLMUSDT",
+]
+
+fetch_binance_data(
+        assets=assets, 
+        tf=Client.KLINE_INTERVAL_5MINUTE, 
+        days="3 day ago UTC",
+        save_location="data")
+
+df = pd.read_parquet("data/binance-ETHUSDT-5m.parquet")
+print(df)
+
